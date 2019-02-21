9184edbba2850348212cff3d2383408e12145419
Ritesh shivdas kagale <rites.kagale@gmail.com>
2019-02-21 15:56:21 +0530

    GET /todos/id

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {ObjectID} = require("mongodb");
 
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/todo");
@@ -19,13 +20,30 @@ app.post('/todos', (req, res) => {
     });
 });
 
-// app.get('/todos', (req, res) => {
-//     Todo.find().then(result => {
-//         res.send({result});
-//     },(error) => {
-//         res.status(400).send(error);
-//     });
-// });
+app.get('/todos', (req, res) => {
+    Todo.find().then(result => {
+        res.send({result});
+    },(error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id
+
+    if (!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then(result => {
+        if (!result){
+            res.status(404).send();
+        }
+        res.send({result});
+    }).catch(err => {
+        res.status(400).send(err);
+    });
+});
 
 app.listen(3000, () => {
     console.log("starting on port 3000");
